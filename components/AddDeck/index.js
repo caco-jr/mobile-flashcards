7423676ef57e8bd3a3b0b9aff626d4656961b457
Carlos Costa <jr.carlosad@gmail.com>
2018-01-25 18:43:32 -0200

    feat: AddDeck

--- components/AddDeck/index.js
+++ components/AddDeck/index.js
@@ -1,23 +1,92 @@
 import React, { Component } from 'react'
 import {
   View,
+  KeyboardAvoidingView,
   Text,
+  TextInput,
   StyleSheet,
+  TouchableOpacity,
 } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { red, gray, darkGray, white, blue, pink } from '../../utils/colors'
 
 class AddDeck extends Component {
+  state = {
+    input: '',
+  }
+
+  onChangeText = (input) => {
+    return this.setState({ input })
+  }
+
+  //TODO: Fazer o método onPress
+
   render() {
+    const { input } = this.state;
+
     return (
-      <View style={styles.center}>
-        <Text> Add Deck </Text>
-      </View>
+      <KeyboardAvoidingView behavior='padding' style={styles.container}>
+        <View style={styles.center}>
+          <MaterialCommunityIcons color={blue} size={80} name='library-books' />
+          <Text style={styles.text}>Create a new Deck</Text>
+        </View>
+
+        <View style={styles.sameLine} >
+          <TextInput
+            value={input}
+            onChangeText={this.onChangeText}
+            placeholder="Ex: React"
+            style={styles.input}
+            placeHolder='Deck Label' />
+
+          <TouchableOpacity
+            style={[styles.button, styles.center]}>
+            <Text style={styles.buttonText}>CREATE</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
     )
   }
 }
+
 const styles = StyleSheet.create({
+  text: {
+    color: darkGray,
+    fontSize: 24,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    padding: 15,
+  },
   center: {
     justifyContent: 'center',
     alignItems: 'center',
   },
+  sameLine: {
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  input: {
+    height: 40,
+    flex: 2,
+    marginLeft: 1,
+    marginRight: 1,
+    fontSize: 16,
+    borderBottomWidth: 1,
+    borderColor: gray,
+    padding: 10,
+  },
+  button: {
+    backgroundColor: pink,
+    height: 40,
+    flex: 1,
+    borderTopRightRadius: 6,
+  },
+  buttonText: {
+    color: white,
+  },
 })
+
 export default AddDeck
