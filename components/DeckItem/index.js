bb624858ab49e01589147b4f0b08f0ea724ad5fd
Carlos Costa <jr.carlosad@gmail.com>
2018-01-25 19:24:09 -0200

    feat: Decks and DeckItem

---
+++ components/DeckItem/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { white, red, darkGray } from '../../utils/colors'
+import { Ionicons } from '@expo/vector-icons'
+
+export const DeckItem = ({ deck, nav, screenProps }) => {
+    return (
+        <TouchableOpacity style={styles.itemCont}
+            onPress={() => nav.navigate('DeckItemDetails', { deck: deck.item })}>
+            <View style={styles.icon}>
+                <Ionicons style={styles.icon} color={white} size={60} name='ios-card-outline' />
+            </View>
+            <View style={styles.info}>
+                <Text style={styles.titleText}>{deck.item.title}</Text>
+            </View>
+            <View style={styles.endMenu}>
+                <TouchableOpacity
+                    onPress={() => screenProps.removeDeck(deck.item.title)}>
+                    <Ionicons style={{ margin: 4 }} color={white} size={30} name='ios-trash-outline' />
+                </TouchableOpacity>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    itemCont: {
+        flex: 1,
+        height: 100,
+        flexDirection: 'row',
+        backgroundColor: red,
+        borderTopWidth: 1,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderColor: darkGray,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    icon: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 80,
+        padding: 8,
+    },
+    info: {
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+    },
+    endMenu: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    titleText: {
+        color: white,
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    subTitle: {
+        color: white,
+        borderWidth: 1,
+        borderRadius: 8,
+        padding: 8,
+        margin: 4,
+        borderColor: white,
+    },
+})
--- components/Decks/index.js
+++ components/Decks/index.js
@@ -1,12 +1,46 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+import { DeckItem } from '../DeckItem'
+import { gray, red, white } from '../../utils/colors'
+import { FontAwesome } from '@expo/vector-icons'
 
-const Decks = () => {
+const renderListItem = ({ deck, navigation, screenProps }) => {
+  return <DeckItem nav={navigation} screenProps={screenProps} deck={deck} />
+}
+const keyExtractor = (item, idx) => {
+  return idx
+}
+
+const Decks = ({ navigation, screenProps }) => {
+  const { decks } = screenProps
   return (
-    <View style={{ flex: 1 }}>
-      <Text> Olá </Text>
-    </View>
+    decks && decks.length
+      ? <View style={{ flex: 1 }}>
+        <View style={{ flex: 2 }}>
+          <FlatList
+            data={decks}
+            renderItem={(deck) => renderListItem({ deck, navigation, screenProps })}
+            keyExtractor={keyExtractor}
+          />
+        </View>
+      </View>
+      : <Text> Add a New Deck </Text>
   )
 }
 
+styles = StyleSheet.create({
+  decks: {
+    flex: 1,
+  },
+  titleCard: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: white,
+    borderColor: gray,
+    borderWidth: 4,
+    borderRadius: 4,
+    margin: 3,
+  }
+})
 export default Decks
