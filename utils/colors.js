2c6d30dc85adae5abaa1738be87d155cc55a518c
Carlos Costa <jr.carlosad@gmail.com>
2018-01-26 00:55:45 -0200

    style: New gray

--- utils/colors.js
+++ utils/colors.js
@@ -1,6 +1,6 @@
 export const white = '#FFF'
 export const black = '#3e4149'
-export const gray = '#34374C'
+export const gray = '#6d819c'
 export const lightGray = '#D7DCE4'
 export const darkGray = '#2C2E3E'
 export const blue = '#1ec0ff'
