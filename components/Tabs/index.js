4e81fc7500a9c66386d6a5298a6af756d2439a76
Carlos Costa <jr.carlosad@gmail.com>
2018-01-26 03:39:50 -0200

    refact: Indentation

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -6,36 +6,36 @@ import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'
 import { white, blue, lightGray } from '../../utils/colors'
 
 const tabOptions = {
-  navigationOptions: {
-    header: null,
-  },
-  animationEnabled: true,
-  tabBarOptions: {
-    activeTintColor: blue,
-    inactiveTintColor: lightGray,
-    style: {
-      height: 56,
-      padding: 4,
-      backgroundColor: white,
+    navigationOptions: {
+        header: null,
     },
-  },
+    animationEnabled: true,
+    tabBarOptions: {
+        activeTintColor: blue,
+        inactiveTintColor: lightGray,
+        style: {
+            height: 56,
+            padding: 4,
+            backgroundColor: white,
+        },
+    },
 }
 
 export default Tabs = TabNavigator({
-  Decks: {
-    screen: Decks,
-    navigationOptions: {
-      tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons color={tintColor} size={30} name='cards' />,
-      tabBarLabel: 'Decks',
-      title: 'Decks',
+    Decks: {
+        screen: Decks,
+        navigationOptions: {
+            tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons color={tintColor} size={30} name='cards' />,
+            tabBarLabel: 'Decks',
+            title: 'Decks',
+        },
     },
-  },
-  AddDeck: {
-    screen: AddDeck,
-    navigationOptions: {
-      tabBarIcon: ({ tintColor }) => <MaterialIcons color={tintColor} size={30} name='library-add' />,
-      tabBarLabel: 'Add Deck',
-      title: 'Add Deck',
+    AddDeck: {
+        screen: AddDeck,
+        navigationOptions: {
+            tabBarIcon: ({ tintColor }) => <MaterialIcons color={tintColor} size={30} name='library-add' />,
+            tabBarLabel: 'Add Deck',
+            title: 'Add Deck',
+        },
     },
-  },
 }, tabOptions)
